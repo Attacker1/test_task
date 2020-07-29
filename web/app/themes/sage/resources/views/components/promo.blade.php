4a9c6401252b71d6be314c5c2d6551ae5525b3ce
Valeriy <example@gmail.com>
2020-07-29 14:18:49 +0300

    Nav panel

--- web/app/themes/sage/resources/views/components/promo.blade.php
+++ web/app/themes/sage/resources/views/components/promo.blade.php
@@ -4,22 +4,23 @@
         @include('components.promo_wrapper')
     </div>
     <div class="devider"></div>
+    @php
+        $terms = get_terms([
+            'taxonomy' => 'kitchen',
+            'hide_empty' => false, 
+        ]);
+    @endphp
+    @if($terms)
         <div class="shadow">
         <div class="container">
             <div class="promo__navigation">
+                
                 <div class="promo__list">
-                    @php
-                        $terms = get_terms([
-                            'taxonomy' => 'kitchen',
-                            'hide_empty' => false, 
-                        ]);
-                    @endphp
-                    @if($terms)
                         @foreach($terms as $term)
                             <div class='promo__type'>{!! $term->name !!}</div>
                         @endforeach
-                    @endif
                 </div>
+                
                 <div class="promo__more">Еще
                     <div class="dav">
                         <span></span>
@@ -37,4 +38,5 @@
             </div>
         </div>
     </div>
+    @endif
 </section>
